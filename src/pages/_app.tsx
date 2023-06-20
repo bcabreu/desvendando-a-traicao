daaab7920061f52d29b8d4b243e906daaeb98563
bcabreu <bcabreu@icloud.com>
2023-06-20 00:38:20 -0300

    retirada do google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,8 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
-import { initGA, logPageView } from "../../analytics";
-import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    initGA();
-    logPageView();
-  }, []);
-
   return (
     <>
       <Component {...pageProps} />
